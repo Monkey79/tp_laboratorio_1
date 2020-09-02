d05b5e7bea3b13b07e2be82a00c3e2e1d0b80168
Monkey79 <marianomartin79@gmail.com>
2020-09-02 12:01:19 -0300

    preparacion ambiente tps

---
+++ Tp2MDangelo/src/Tp2MDangelo.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Tp2MDangelo.c
+ Author      : mdangelo
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello Tp2"); /* prints Hello Tp2 */
+	return EXIT_SUCCESS;
+}
---
+++ Tp4MDangelo/src/Tp4MDangelo.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Tp4MDangelo.c
+ Author      : mdangelo
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello Tp4"); /* prints Hello Tp4 */
+	return EXIT_SUCCESS;
+}
